859e140c5c9d761ed094b847865573c9263ef1fd
nukomaru <traious5@yahoo.co.jp>
2018-01-10 01:46:37 +0900

    [update] menu外を押した場合にopenを反転させる処理に更新

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -11,24 +11,16 @@ export default class NavBar extends Component {
         return (
             <MuiThemeProvider>
                 <div>
-                    <Drawer
-                        docked={false}
-                        width={200}
-                        open={this.props.open}
-                        onClick={this.props.onToggle}
-                    >
-                        <MenuItem onClick={this.props.onToggle}>React</MenuItem>
+                    <Drawer docked={false} width={200} open={this.props.open} onRequestChange={this.props.onToggle}>
+                        <MenuItem>React</MenuItem>
                         <MenuItem>Redux</MenuItem>
                         <MenuItem>React Router</MenuItem>
                         <MenuItem>Material UI</MenuItem>
                         <MenuItem>Electron</MenuItem>
                     </Drawer>
-                    <AppBar
-                        title="React Study!!!"
-                        onLeftIconButtonClick={this.props.onToggle}
-                    />
-                    </div>
-                </MuiThemeProvider>
+                    <AppBar title="React Study!!!" onLeftIconButtonClick={this.props.onToggle}/>
+                </div>
+            </MuiThemeProvider>
         );
     }
 }
