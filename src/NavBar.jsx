3e04227f444d4c57e234faf7953649eab6017786
nukomaru <traious5@yahoo.co.jp>
2018-01-10 18:41:20 +0900

    [update] インデントの更新

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -3,24 +3,24 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { AppBar, MenuItem, Drawer } from 'material-ui';
 
 export default class NavBar extends Component {
-    constructor(props){
-        super(props);
-    }
+	constructor(props){
+		super(props);
+	}
 
-    render() {
-        return (
-            <MuiThemeProvider>
-                <div>
-                    <Drawer docked={false} width={200} open={this.props.open} onRequestChange={this.props.onToggle}>
-                        <MenuItem>React</MenuItem>
-                        <MenuItem>Redux</MenuItem>
-                        <MenuItem>React Router</MenuItem>
-                        <MenuItem>Material UI</MenuItem>
-                        <MenuItem>Electron</MenuItem>
-                    </Drawer>
-                    <AppBar title="React Study!!!" onLeftIconButtonClick={this.props.onToggle}/>
-                </div>
-            </MuiThemeProvider>
-        );
-    }
+	render() {
+		return (
+			<MuiThemeProvider>
+				<div>
+					<Drawer docked={false} width={200} open={this.props.open} onRequestChange={this.props.onToggle}>
+						<MenuItem>React</MenuItem>
+						<MenuItem>Redux</MenuItem>
+						<MenuItem>React Router</MenuItem>
+						<MenuItem>Material UI</MenuItem>
+						<MenuItem>Electron</MenuItem>
+				</Drawer>
+				<AppBar title="React Study!!!" onLeftIconButtonClick={this.props.onToggle}/>
+				</div>
+			</MuiThemeProvider>
+		);
+	}
 }
